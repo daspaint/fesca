1ee7befb0083125b60d1779e1eaba0b08597811c
mmendoughe <m.mendoughe@hotmail.de>
2025-06-12 18:20:43 +0200

    Different execution branches depending on roles

--- fesca/src/main.rs
+++ fesca/src/main.rs
@@ -2,5 +2,21 @@ use helpers::read_config::read_config;
 
 fn main() {
     let role = read_config("config.txt", "role").unwrap_or_else(|| "None".to_string());
-    println!("FESCA is here {:?}", role);
+    println!("FESCA is here with role: {}", role);
+
+    match role.as_str() {
+        "data_owner" => {
+            println!("Initializing data owner node...");
+        }
+        "computing_node" => {
+            println!("Initializing computing node...");
+        }
+        "data_analyst" => {
+            println!("Initializing data analyst node...");
+        }
+        _ => {
+            eprintln!("Error: Invalid role '{}'. Must be one of: data_owner, computing_node, data_analyst", role);
+            std::process::exit(1);
+        }
+    }
 }

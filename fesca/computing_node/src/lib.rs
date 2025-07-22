f3b961c7f891246af800e45ed4d098bef4735c38
Mendoughe Mimoun <mmimoun@zs01.lab.dm.informatik.tu-darmstadt.de>
2025-07-22 09:40:36 +0000

    Added receive and storage modules

--- fesca/computing_node/src/lib.rs
+++ fesca/computing_node/src/lib.rs
@@ -1,2 +1,13 @@
 pub mod helpers;
 pub mod node;
+
+// Receive module components
+pub mod receive {
+    pub mod server;
+    pub mod storage;
+}
+
+// Re-export main functionality
+pub use node::Node;
+pub use receive::server::{ShareReceiver, start_server};
+pub use receive::storage::BinaryShareStorage;

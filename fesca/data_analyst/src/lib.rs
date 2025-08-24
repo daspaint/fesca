d7e6b036443dc1bdd571c982a4877ed5a048b84b
Darya Igonina <daria.vitaly@gmail.com>
2025-08-24 18:28:32 +0200

    removing the binary table reader from data_analyst, moving it to computing_node

--- fesca/data_analyst/src/lib.rs
+++ fesca/data_analyst/src/lib.rs
@@ -4,17 +4,17 @@ mod sql_to_logical;
 mod logical_to_circuits;
 mod circuit_builder;
 
-mod local_exec;
-mod binary_exec;
-mod row_circuit;
-mod run_partsupp;
+// mod local_exec;
+// mod binary_exec;
+// mod row_circuit;
+// mod run_partsupp;
 
 use anyhow::{Result, bail};
 use log::info;
 // use logical_plan::{Expr as LPExpr, BinaryOperator, LogicalPlan, AggregateFunc};
 use logical_to_circuits::compile_to_circuit;
 use sql_to_logical::sql_to_logical_plan;
-use local_exec::{Catalog, execute, ExecResult, Cell}; // TODO: comment or delete when sql optimizing is done
+// use local_exec::{Catalog, execute, ExecResult, Cell}; // TODO: comment or delete when sql optimizing is done
 
 // use sqlparser::dialect::GenericDialect;
 // use sqlparser::parser::Parser;
@@ -44,22 +44,25 @@ pub fn run() -> Result<()> {
     /*
     Fully working code for translating phPlan into circuit, uncomment when sql optimizing is done
      */
-    // // Build circuit for e.g. 5 rows × 2 columns. Improvement idea: read table size dynamically from existing dataset.
-    // // Better: estimate how big is the row, and apply the same function to each row.
-    // let circuit = compile_to_circuit(&logical, 5, 2);
-    // info!("Circuit wire_count = {}", circuit.wire_count);
-    // info!("Circuit gates count = {}", circuit.gates.len());
-    // info!("Circuit outputs = {:?}", circuit.outputs);
+    // Build circuit for e.g. 5 rows × 2 columns. Improvement idea: read table size dynamically from existing dataset.
+    // Better: estimate how big is the row, and apply the same function to each row.
+    let circuit = compile_to_circuit(&logical, 5, 2);
+    info!("Circuit wire_count = {}", circuit.wire_count);
+    info!("Circuit gates count = {}", circuit.gates.len());
+    info!("Circuit outputs = {:?}", circuit.outputs);
 
-    // //log the circuit structure
-    // info!("Circuit gates: {:#?}", circuit.gates);
+    //log the circuit structure
+    info!("Circuit gates: {:#?}", circuit.gates);
 
-    // // Log each gate
-    // for g in &circuit.gates {
-    //     info!("Gate: {:?}", g);
-    // }
+    // Log each gate
+    for g in &circuit.gates {
+        info!("Gate: {:?}", g);
+    }
 
-    run_partsupp::run_partsupp(None, None)?;
+    /*
+    Runs the binary executor on a test table, not 100% working yet
+     */
+    // run_partsupp::run_partsupp(None, None)?;
 
     Ok(())
 }
